50d5d0aa7c25e80ce1a71d560914081aaa70aa49
AI-sangbeom <sb486868@gmail.com>
2024-12-06 19:25:31 +0900

    hello

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
-import socket
+import os
 import time
-#from pynput import keyboard
-
-esp32_ip = '192.168.2.218'  # ESP32의 IP 주소
+import timm
+import serial
+import torch
+import torch.nn as nn
+import numpy as np
+import socket
+from torchvision import  transforms
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from huggingface_hub import hf_hub_download
+from ultralytics import YOLO
+from supervision import Detections
+esp32_ip = '192.168.146.119'  # ESP32의 IP 주소
 esp32_port = 8080  # ESP32에서 설정한 포트
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((esp32_ip, esp32_port))
-
-print('connected')
-
-while True:
-
-    # ESP32에서 받은 데이터 수신
-    # client_socket.send('Hello, ESP32!'.encode())
-    data = client_socket.recv(1024)
-    # if not(len(data.decode()) ==37):
-    #     pass
-    # else:
-    client_socket.send('Hello, ESP32! \n'.encode())
-    print("Received from ESP32:", data.decode())
-
-
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # self.client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1) 
+    client_socket.connect((esp32_ip, esp32_port))
+    # time.sleep(1)  # 시리얼 연결 대기
+    print('ESP32 Connected')
+except serial.SerialException as e:
+    print(f"Could not open serial port: {e}")
